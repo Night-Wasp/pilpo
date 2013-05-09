762341de664e5a8a09f60d33046a0c9407f0e2f5
Andrew Collings <andy@pixel6design.com>
2013-05-09 17:14:35 +0100

    Pilpo creation and status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilpo::Application.config.secret_token = 'be8e0c0f60f72235fa1e21325ce4f7a45a8ef5769fc81b9dde1bda272834df23f36608850c35c5d6b808fb1f1ec4e0255508ad0bbbb4a1b23e613d1c891bac58'
